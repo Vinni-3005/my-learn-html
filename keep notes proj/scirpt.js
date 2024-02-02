aad1eccce47e46c0c3cbc65ee96393c4f69d7e0b
Vinni-3005 <vinithaaduri@gmail.com>
2024-02-02 10:24:18 +0530

    kepp notes proj

---
+++ keep notes proj/scirpt.js
@@ -0,0 +1,8 @@
+
+
+    const addBox = document.querySelector(".add-box");
+    const popupBox = document.querySelector(".popup-box");
+
+    addBox.addEventListener( "click", () => {
+        popupBox.classList.add("show");
+    });
